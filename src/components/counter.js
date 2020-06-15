c0b05db827b77cb23312da618dde0d402ed8a4d1
Joel Davis <joeladavisdev@gmail.com>
2020-06-15 13:21:06 -0600

    Fix node_modules

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,26 +1,44 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Counter extends Component {
-    constructor() {
-        super();
-        this.state = {
-            number: 0
-        }
-    }
-
-    handleClick = (count) => {
-        this.setState(prevState => ({number: prevState.number + count}))
-    }
-
-    render() {
-        return (
-            <div className="wrapper">
-                <h1>Counter</h1>
-                <button className="btn" onClick={() => this.handleClick(-1)}>Count down</button>
-                <button className="btn" onClick={() => this.handleClick(1)}>Count up</button>
-                <div style={{number: this.state.number}}>{this.state.number}</div>
-            </div>
-        )
-    }
+export default function Counter() {
+    const [count, setCount] = useState(0);
+    
+    return (
+        <div className="wrapper">
+            <h1>Counter</h1>
+            <button className="btn" onClick={() => setCount(count - 1)}>Count down</button>
+            <button className="btn" onClick={() => setCount(count + 1)}>Count up</button>
+            <div style={{count}}>{count}</div>
+        </div>
+    )
+    
 }
-export default Counter
+
+// import React, { Component } from "react";
+
+// class Counter extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             number: 0
+//         }
+//     }
+
+//     handleClick = (count) => {
+//         this.setState(prevState => ({number: prevState.number + count}))
+//     }
+
+//     render() {
+//         return (
+//             <div className="wrapper">
+//                 <h1>Counter</h1>
+//                 <button className="btn" onClick={() => this.handleClick(-1)}>Count down</button>
+//                 <button className="btn" onClick={() => this.handleClick(1)}>Count up</button>
+//                 <div style={{number: this.state.number}}>{this.state.number}</div>
+//             </div>
+//         )
+//     }
+// }
+// export default Counter
+
+
