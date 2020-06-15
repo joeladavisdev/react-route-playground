11c8ede6db9b04ed5e8f930d80b6d20d83acb8e2
Joel Davis <joeladavisdev@gmail.com>
2020-06-15 13:24:54 -0600

    Counter Initial state

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 export default function Counter() {
     const [count, setCount] = useState(0);
+    //const [stateName, stateUpdateFunctionName] = Initial state
     
     return (
         <div className="wrapper">
